3af0db79fac798a93354e0c8e7de87c41d2d1b5a
yetone <yetoneful@gmail.com>
2022-02-28 17:05:59 +0800

    feat: incremental selection

--- lua/layers/editor/configs.lua
+++ lua/layers/editor/configs.lua
@@ -177,6 +177,16 @@ function configs.treesitter()
         enable = true
       },
 
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+
       textsubjects = {
         enable = true,
         prev_selection = ',', -- (Optional) keymap to select the previous selection
