1afde864c68d0f132c6e1db8bc5812d358b4d0e1
yetone <yetoneful@gmail.com>
2022-04-24 01:45:11 +0800

    optimize: going to last cursor position with center the buffer and avoid matching git commit editmsg

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -83,10 +83,10 @@ hi Pmenu ctermfg=white ctermbg=238
 ]]
 
 vim.cmd [[
-" Uncomment the following to have Vim jump to the last position when
-" reopening a file
-if has("autocmd")
-  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-endif
+augroup last_cursor_position
+  autocmd!
+  autocmd BufReadPost *
+    \ if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit' | execute "normal! g`\"zvzz" | endif
+augroup END
 ]]
 
