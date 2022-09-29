6f366c9e26e0bc64a0087ae3bb0bb4ec494f5cdd
yetone <yetoneful@gmail.com>
2022-09-29 05:45:12 +0000

    feat: support for yanking via OSC52

--- lua/layers/editor/settings.lua
+++ lua/layers/editor/settings.lua
@@ -1,3 +1,12 @@
 vim.cmd [[
 command Format :lua vim.lsp.buf.formatting()
+
+function Copy()
+  let c = join(v:event.regcontents,"\n")
+  let c64 = system("base64", c)
+  let s = "\e]52;c;" . trim(c64) . "\x07"
+  call chansend(v:stderr, s)
+endfunction
+
+autocmd TextYankPost * call Copy()
 ]]
