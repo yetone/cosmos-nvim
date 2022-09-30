ccde51761476c938a37bc84b71e6006eb7217dac
yetone <yetoneful@gmail.com>
2022-09-30 04:17:17 +0000

    feat: use lua method to support osc52

--- lua/layers/editor/settings.lua
+++ lua/layers/editor/settings.lua
@@ -1,12 +1,24 @@
 vim.cmd [[
 command Format :lua vim.lsp.buf.formatting()
-
-function Copy()
-  let c = join(v:event.regcontents,"\n")
-  let c64 = system("base64", c)
-  let s = "\e]52;c;" . trim(c64) . "\x07"
-  call chansend(v:stderr, s)
-endfunction
-
-autocmd TextYankPost * call Copy()
 ]]
+
+local function copy(sel)
+  return function()
+    local content = table.concat(vim.v.event.regcontents, '\n')
+    local encoded_content = vim.fn.system('base64', content)
+    local osc = '\027]52;' .. sel .. ';' .. encoded_content .. '\a'
+    if vim.fn.filewritable('/dev/fd/2') then
+      vim.fn.writefile({osc}, '/dev/fd/2', 'b')
+    else
+      vim.fn.chansend(vim.v.stderr, osc)
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("YankOSC52", { clear = true }),
+  pattern = "*",
+  callback = function()
+    copy('c')()
+  end,
+})
