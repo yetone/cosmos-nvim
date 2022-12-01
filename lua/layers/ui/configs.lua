286c40d2de7a9abd3eb876287fe304332e3efb37
yetone <yetoneful@gmail.com>
2022-12-01 15:00:27 +0000

    fix: icon padding in bufferline

--- lua/layers/ui/configs.lua
+++ lua/layers/ui/configs.lua
@@ -343,8 +343,8 @@ function configs.bufferline()
       diagnostics_indicator = function(count, level, diagnostics_dict, context)
         local s = " "
         for e, n in pairs(diagnostics_dict) do
-          local sym = e == "error" and " "
-          or (e == "warning" and " " or " " )
+          local sym = e == "error" and " "
+          or (e == "warning" and " " or " " )
           s = s .. n .. sym
         end
         return s
