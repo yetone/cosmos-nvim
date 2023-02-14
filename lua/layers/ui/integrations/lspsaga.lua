28fcc2c02a13a896b4b11418e866a267f7960ed0
yetone <yetoneful@gmail.com>
2023-02-14 09:09:58 +0000

    feat(ui): add lspsaga highlights

---
+++ lua/layers/ui/integrations/lspsaga.lua
@@ -0,0 +1,25 @@
+local colors = require('base46').get_theme_tb('base_30')
+
+return {
+  -- LspSaga
+  LspSagaDiagnosticBorder = {
+    fg = colors.darker_black,
+    bg = colors.darker_black,
+  },
+  LspSagaDiagnosticTruncateLine = {
+    fg = colors.darker_black,
+    bg = colors.darker_black,
+  },
+  LspSagaRenameBorder = {
+    fg = colors.darker_black,
+    bg = colors.darker_black,
+  },
+  LspSagaHoverBorder = {
+    fg = colors.darker_black,
+    bg = colors.darker_black,
+  },
+  LspSagaDefPreviewBorder = {
+    fg = colors.darker_black,
+    bg = colors.darker_black,
+  },
+}
