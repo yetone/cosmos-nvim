f66307053cdccdda6fe03dc87197c9187dd890eb
yetone <yetoneful@gmail.com>
2023-02-14 07:10:07 +0000

    fix(ui): bufferline highlights

--- lua/layers/ui/configs.lua
+++ lua/layers/ui/configs.lua
@@ -310,13 +310,12 @@ function configs.fidget()
   })
 end
 
-function configs.bufferline()
-  local bufferline = require('bufferline')
+function configs.get_bufferline_options()
   local default = {
     colors = require('layers.ui.colors').get().base_30,
   }
 
-  bufferline.setup({
+  return {
     options = {
       always_show_bufferline = false,
       diagnostics = 'nvim_lsp',
@@ -330,7 +329,206 @@ function configs.bufferline()
       end,
       offsets = { { filetype = 'NvimTree', text = '', padding = 1 } },
     },
-  })
+
+    highlights = {
+      background = {
+        fg = default.colors.grey_fg,
+        bg = default.colors.black2,
+      },
+
+      -- buffers
+      buffer_selected = {
+        fg = default.colors.white,
+        bg = default.colors.black,
+        bold = true,
+      },
+      buffer_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+
+      -- for diagnostics = "nvim_lsp"
+      diagnostic = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      hint = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      hint_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      hint_diagnostic = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      hint_diagnostic_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      info = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      info_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      info_diagnostic = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      info_diagnostic_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      error = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      error_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      error_diagnostic = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      error_diagnostic_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      warning = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      warning_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      warning_diagnostic = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      warning_diagnostic_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+
+      buffer = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+
+      numbers = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+
+      numbers_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+
+      duplicate = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+
+      duplicate_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black,
+      },
+
+      -- close buttons
+      close_button = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      close_button_visible = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      close_button_selected = {
+        fg = default.colors.red,
+        bg = default.colors.black,
+      },
+      fill = {
+        fg = default.colors.grey_fg,
+        bg = default.colors.black2,
+      },
+      indicator_selected = {
+        fg = default.colors.black,
+        bg = default.colors.black,
+      },
+
+      -- modified
+      modified = {
+        fg = default.colors.red,
+        bg = default.colors.black2,
+      },
+      modified_visible = {
+        fg = default.colors.red,
+        bg = default.colors.black2,
+      },
+      modified_selected = {
+        fg = default.colors.green,
+        bg = default.colors.black,
+      },
+
+      -- separators
+      separator = {
+        fg = default.colors.black2,
+        bg = default.colors.black2,
+      },
+      separator_visible = {
+        fg = default.colors.black2,
+        bg = default.colors.black2,
+      },
+      separator_selected = {
+        fg = default.colors.black2,
+        bg = default.colors.black2,
+      },
+      tab_separator = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      tab_separator_selected = {
+        fg = default.colors.light_grey,
+        bg = default.colors.black2,
+      },
+      group_separator = {
+        fg = default.colors.black2,
+        bg = default.colors.black2,
+      },
+      offset_separator = {
+        fg = default.colors.black2,
+        bg = default.colors.black2,
+      },
+
+      -- tabs
+      tab = {
+        fg = default.colors.light_grey,
+        bg = default.colors.one_bg3,
+      },
+      tab_selected = {
+        fg = default.colors.black2,
+        bg = default.colors.nord_blue,
+      },
+      tab_close = {
+        fg = default.colors.red,
+        bg = default.colors.black,
+      },
+    },
+  }
+end
+
+function configs.bufferline()
+  local bufferline = require('bufferline')
+
+  bufferline.setup(configs.get_bufferline_options())
 end
 
 function configs.neoscroll()
--- lua/layers/ui/functions.lua
+++ lua/layers/ui/functions.lua
@@ -6,6 +6,17 @@ local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 local Previewer = require('telescope.previewers').Previewer
 
+function M.preview_theme(theme)
+  local options = require('layers.ui.options')
+  options.theme = theme
+  package.loaded['layers.ui.colors' or false] = nil
+  require('layers.ui.colors').setup()
+  require('lualine.highlight').create_highlight_groups(require('layers.ui.highlights').get_lualine_theme())
+  require('bufferline.config').set(require('layers.ui.configs').get_bufferline_options())
+  require('bufferline.highlights').reset_icon_hl_cache()
+  require('bufferline.highlights').set_all(require('bufferline.config').update_highlights())
+end
+
 function M.pick_theme(opts)
   opts = opts or {}
 
@@ -29,45 +40,33 @@ function M.pick_theme(opts)
   local current_theme = vim.g.colors_name
 
   pickers
-    .new(opts, {
-      prompt_title = 'Pick Theme',
-      finder = finders.new_table({
-        results = themes,
-      }),
-      default_selection_index = default_selection_index,
-      sorter = conf.generic_sorter(opts),
-      previewer = Previewer:new({
-        setup = function(self)
-          local selection = action_state.get_selected_entry()
-          local options = require('layers.ui.options')
-          options.theme = selection.value
-          package.loaded['layers.ui.colors' or false] = nil
-          require('layers.ui.colors').setup()
-          require('lualine.highlight').create_highlight_groups(require('layers.ui.highlights').get_lualine_theme())
+      .new(opts, {
+        prompt_title = 'Pick Theme',
+        finder = finders.new_table({
+          results = themes,
+        }),
+        default_selection_index = default_selection_index,
+        sorter = conf.generic_sorter(opts),
+        previewer = Previewer:new({
+          setup = function(self)
+            local selection = action_state.get_selected_entry()
+            M.preview_theme(selection.value)
+          end,
+          teardown = function(self)
+            M.preview_theme(current_theme)
+          end,
+          preview_fn = function(self, entry) end,
+        }),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            M.preview_theme(selection.value)
+          end)
+          return true
         end,
-        teardown = function(self)
-          local options = require('layers.ui.options')
-          options.theme = current_theme
-          package.loaded['layers.ui.colors' or false] = nil
-          require('layers.ui.colors').setup()
-          require('lualine.highlight').create_highlight_groups(require('layers.ui.highlights').get_lualine_theme())
-        end,
-        preview_fn = function(self, entry) end,
-      }),
-      attach_mappings = function(prompt_bufnr, map)
-        actions.select_default:replace(function()
-          actions.close(prompt_bufnr)
-          local selection = action_state.get_selected_entry()
-          local options = require('layers.ui.options')
-          options.theme = selection.value
-          package.loaded['layers.ui.colors' or false] = nil
-          require('layers.ui.colors').setup()
-          require('lualine.highlight').create_highlight_groups(require('layers.ui.highlights').get_lualine_theme())
-        end)
-        return true
-      end,
-    })
-    :find()
+      })
+      :find()
 end
 
 return M
